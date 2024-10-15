c43efdad78cfbc621dc37af3c2092d81e937e206
Saiyam Yadav <ydvsaiyam001@gmail.com>
2024-10-15 22:50:24 +0530

    Create user controller (login)

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -45,3 +45,58 @@ export const register = async (req, res) => {
     return res.status(500).json({ message: "Server error", success: false });
   }
 };
+
+// Login
+export const login = async (req, res) => {
+  try {
+    // Get data
+    const { email, password, role } = req.body;
+
+    // Validate data
+    const fields = [email, password, role];
+    if (fields.some((field) => field === "")) {
+      return res
+        .status(400)
+        .json({ message: "All fields are required", success: false });
+    }
+
+    // Check DB if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User not found", success: false });
+    }
+
+    // Encrypt password
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ message: "Credentials are wrong", success: false });
+    }
+
+    const tokenData = {
+      userId: user._id,
+    };
+
+    const token = jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: `${user.fullName} Logged in successfully`,
+        success: true,
+      });
+  } catch (error) {
+    console.error("Login error:", error);
+    return res.status(500).json({ message: "Server error", success: false });
+  }
+};
