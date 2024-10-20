0b60ee39898bc6bbf5e591584e52be4d9a8fb3fc
Saiyam Yadav <ydvsaiyam001@gmail.com>
2024-10-20 22:26:19 +0530

    Create signup component

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,9 +1,76 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>
-      <h1>signup</h1>
+    <div className="max-w-md mx-auto shadow-2xl rounded-lg ">
+      <form className="p-5">
+        <div className="flex flex-col gap-4">
+          {/* Form title */}
+          <div>
+            <h4 className="text-xl font-bold">Sign Up!</h4>
+          </div>
+
+          {/* Form Fields */}
+          <div className="flex flex-col gap-3">
+            {/* FullName */}
+            <div>
+              <label>Fullname</label>
+              <Input type="text" placeholder="John Doe" />
+            </div>
+
+            {/* Email */}
+            <div>
+              <label>Email</label>
+              <Input type="text" placeholder="john@gmail.com" />
+            </div>
+
+            {/* Phone Number */}
+            <div>
+              <label>Phone Number</label>
+              <Input type="text" placeholder="XXXXXXXX01" />
+            </div>
+
+            {/* Password */}
+            <div>
+              <label>Password</label>
+              <Input type="password" placeholder="********" />
+            </div>
+
+            {/* Radio */}
+            <div>
+              <div>
+                <input type="radio" name="role" id="student" />
+                <label className="ml-2">Student</label>
+              </div>
+              <div>
+                <input type="radio" name="role" id="recruiter" />
+                <label className="ml-2">Recruiter</label>
+              </div>
+            </div>
+
+            {/* File */}
+            <div>
+              <label>Profile</label>
+              <Input type="file" accept="image/*" />
+            </div>
+
+            {/* Submit */}
+            <Button className="mt-3">Sign up</Button>
+            <span>
+              Already have an accoount?
+              <Link
+                to={"/login"}
+                className="font-semibold text-blue-600 px-2 hover:underline"
+              >
+                Login
+              </Link>
+            </span>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
