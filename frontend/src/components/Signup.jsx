373123daeafa0d14bdf36e7bed57dcba7d680d67
Saiyam Yadav <ydvsaiyam001@gmail.com>
2024-10-21 22:35:00 +0530

    Implement Signup funtionallity

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,12 +1,64 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
-import { Link } from "react-router-dom";
+import { USER_API_END_POINT } from "@/const/constant";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Signup = () => {
+  const [inputData, setInputData] = useState({
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: "",
+  });
+
+  const navigate = useNavigate();
+
+  const changeEventHandler = (e) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value });
+  };
+  const changeFileHandler = (e) => {
+    setInputData({ ...inputData, file: e.target.files?.[0] });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    // console.log(inputData);
+
+    const formData = new FormData();
+    formData.append("fullName", inputData.fullName);
+    formData.append("email", inputData.email);
+    formData.append("phoneNumber", inputData.phoneNumber);
+    formData.append("password", inputData.password);
+    formData.append("role", inputData.role);
+    if (inputData.file) {
+      formData.append("file", inputData.file);
+    }
+    try {
+      const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        withCredentials: true,
+      });
+
+      if (res.data.success) {
+        navigate("/login");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  };
+
   return (
-    <div className="max-w-md mx-auto shadow-2xl rounded-lg ">
-      <form className="p-5">
+    <div className="max-w-md mx-auto shadow-2xl rounded-lg">
+      <form onSubmit={submitHandler} className="p-5">
         <div className="flex flex-col gap-4">
           {/* Form title */}
           <div>
@@ -18,43 +70,92 @@ const Signup = () => {
             {/* FullName */}
             <div>
               <label>Fullname</label>
-              <Input type="text" placeholder="John Doe" />
+              <Input
+                type="text"
+                value={inputData.fullName}
+                name="fullName"
+                onChange={changeEventHandler}
+                placeholder="John Doe"
+              />
             </div>
 
             {/* Email */}
             <div>
               <label>Email</label>
-              <Input type="text" placeholder="john@gmail.com" />
+              <Input
+                type="text"
+                value={inputData.email}
+                name="email"
+                onChange={changeEventHandler}
+                placeholder="john@gmail.com"
+              />
             </div>
 
             {/* Phone Number */}
             <div>
               <label>Phone Number</label>
-              <Input type="text" placeholder="XXXXXXXX01" />
+              <Input
+                type="text"
+                value={inputData.phoneNumber}
+                name="phoneNumber"
+                onChange={changeEventHandler}
+                placeholder="XXXXXXXX01"
+              />
             </div>
 
             {/* Password */}
             <div>
               <label>Password</label>
-              <Input type="password" placeholder="********" />
+              <Input
+                type="password"
+                value={inputData.password}
+                name="password"
+                onChange={changeEventHandler}
+                placeholder="********"
+              />
             </div>
 
             {/* Radio */}
             <div>
               <div>
-                <input type="radio" name="role" id="student" />
-                <label className="ml-2">Student</label>
+                <input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  checked={inputData.role === "student"}
+                  onChange={changeEventHandler}
+                  id="student"
+                  className="cursor-pointer"
+                />
+                <label htmlFor="student" className="ml-2 cursor-pointer">
+                  Student
+                </label>
               </div>
               <div>
-                <input type="radio" name="role" id="recruiter" />
-                <label className="ml-2">Recruiter</label>
+                <input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  checked={inputData.role === "recruiter"}
+                  onChange={changeEventHandler}
+                  id="recruiter"
+                  className="cursor-pointer"
+                />
+                <label htmlFor="recruiter" className="ml-2 cursor-pointer">
+                  Recruiter
+                </label>
               </div>
             </div>
 
             {/* File */}
             <div>
               <label>Profile</label>
-              <Input type="file" accept="image/*" />
+              <Input
+                type="file"
+                accept="image/*"
+                onChange={changeFileHandler}
+                className="cursor-pointer"
+              />
             </div>
 
             {/* Submit */}
